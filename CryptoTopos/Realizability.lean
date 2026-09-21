201a9751578eef7d05583dcc63eb66050568f1bb
Yura Ogura <307669616+yura-ogura@users.noreply.github.com>
2026-09-21 23:24:15 +0900

    Update Realizability.lean

--- CryptoTopos/Realizability.lean
+++ CryptoTopos/Realizability.lean
@@ -7,6 +7,7 @@ import Mathlib.Tactic.Ring
 import Mathlib.CategoryTheory.Opposites
 import Mathlib.CategoryTheory.Yoneda
 import Mathlib.CategoryTheory.Functor.Category
+import CryptoTopos.Model
 
 open CategoryTheory
 
@@ -364,13 +365,6 @@ def Decipher_TrackedMap :
     TrackedMap (prodPA (prodPA Zq_PA Zq_PA) Zq_PA) Zq_PA where
   toFun := decipherFun
   tracked := decipher_isTracked
-
-structure Manifold4D where
-  id : Nat
-  x  : Nat
-  y  : Nat
-  z  : Nat
-  t  : Nat
 
 def encodeManifold4D (m : Manifold4D) : Nat :=
   Nat.pair m.id (Nat.pair m.x (Nat.pair m.y (Nat.pair m.z m.t)))
